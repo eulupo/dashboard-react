a8a83630e38258bf9b834ef51ea2229c66023a9e
ninasamartin <samartinnina@gmail.com>
2021-03-23 16:26:42 -0300

    el logo redirecciona a la home

--- src/components/Sidebar/Brand.js
+++ src/components/Sidebar/Brand.js
@@ -4,7 +4,7 @@ import logo from '../../assets/images/logo-solo.svg';
 
 function Brand() {
     return(
-        <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+        <a className="sidebar-brand d-flex align-items-center justify-content-center" href="http://localhost:3001/">
             <div className="sidebar-brand-icon">
                 <img src={logo} style={{height: "3rem"}} alt="logo championes"/>
             </div>
